c723b11dfc5382769815b36b2d678a7ec43a0671
valdiwav <107816113+valdiwav@users.noreply.github.com>
2025-01-14 20:39:17 -0300

    eslint4

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
+import { configs } from "eslint-plugin-next";
+
 export default [
   {
     files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"], // Archivos a los que se aplica
-    extends: ["next/core-web-vitals"], // Configuración recomendada de Next.js
+    ...configs["core-web-vitals"], // Configuración de Next.js para Core Web Vitals
   },
 ];
