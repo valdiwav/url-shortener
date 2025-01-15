f10b120a3b19210b3c7a2dabb7efe93a133c667c
valdiwav <107816113+valdiwav@users.noreply.github.com>
2025-01-14 23:10:55 -0300

    nextauth/authoptions

---
+++ src/app/utils/authOptions.js
@@ -0,0 +1,65 @@
+import CredentialsProvider from "next-auth/providers/credentials";
+import NextAuth from "next-auth";
+import db from "../libs/db";
+import bcrypt from "bcrypt";
+
+export const authOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        const userFound = await db.user.findUnique({
+          where: { email: credentials?.email },
+        });
+
+        if (!userFound) throw new Error("No user found");
+
+        const matchPassword = await bcrypt.compare(
+          credentials.password,
+          userFound.password
+        );
+
+        if (!matchPassword) throw new Error("Wrong password");
+
+        return {
+          id: userFound.id,
+          name: userFound.name,
+          email: userFound.email,
+        };
+      },
+    }),
+  ],
+  callbacks: {
+    async session({ session, token }) {
+      const user = await db.user.findUnique({
+        where: { id: token.id },
+      });
+
+      if (user) {
+        session.user.id = user.id; // No habrá error aquí
+        session.user.name = user.name;
+        session.user.email = user.email;
+      }
+
+      return session;
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+  },
+  pages: {
+    signIn: "/auth/login",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
